d088b5316f0cf38e2790ffc8ff0ae83d4cc0643f
priti <priti.biyani6@gmail.com>
2016-01-22 11:33:40 +0530

    Downloading tar from network and then installing it.

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 sudo apt-get install openjdk-7-jdk 
 echo "---------------------- java installed--------------------"
-tar xzvf ideaIC-15.0.3.tar.gz
-mv idea-IC-143.1821.5 idea
+wget http://download.jetbrains.com/idea/ideaIC-12.0.4.tar.gz
+idea-IC-123.169
+
+wget -O /tmp/intellij.tar.gz http://download.jetbrains.com/idea/ideaIC-15.0.3.tar.gz && 
+tar xzvf ideaIC-123.169.tar.gz &&
+mv idea-IC-123.169 idea && 
 cd idea/bin
 sh idea.sh
